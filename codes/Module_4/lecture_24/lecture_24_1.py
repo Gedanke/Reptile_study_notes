daaffc990775846d5386f7ee4f45485128633109
Gedanke <13767927306@163.com>
2021-06-23 16:30:57 +0800

    24

--- codes/Module_4/lecture_24/lecture_24_1.py
+++ codes/Module_4/lecture_24/lecture_24_1.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+
+import torch
+
+flag = torch.cuda.is_available()
+if flag:
+    print("CUDA 可使用")
+else:
+    print("CUDA 不可用")
+
+ngpu = 1
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+print("驱动为：", device)
+print("GPU型号： ", torch.cuda.get_device_name(0))
